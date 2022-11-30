e39fd0cc2e8e325ae9db4869384b9ebf7034edfb
janoujan <janoujan@laposte.net>
2022-11-30 07:53:02 +0100

    WIP getUserData function

--- front/js/product.js
+++ front/js/product.js
@@ -28,68 +28,49 @@ const displayKanap = () => {
 // tester que l'"input" et l'"option" sont bien enregistrer
 // une fonction pour ecouter et recuperer les données utilisateurs dans un objet 'userData'
 const userData = {}
-const dataInput = () => {
-  // userData = {}
+const getUserData = () => {
   userData.id = id
   document.getElementById('quantity').addEventListener('input', e => {
     const quantitySelected = e.target.value
     userData.quantity = quantitySelected
-    userData.color = optionColor.value
-    return userData.quantity + userData.color
+    return userData.quantity
   }, false)
-  console.log(userData)
-  return userData.quantity + userData.color
+  document.querySelector('option').addEventListener('value', e => {
+    const colorSelected = e.target.value
+    userData.color = colorSelected
+    return userData.color
+  }, false)
+  // const colorSelected = document.querySelector('option').value
+  // userData.color = colorSelected
+  // return userData.quantity + userData.color
 }
-dataInput()
-console.log(userData)
 
-// tester que le bouton ajoute le produit avec la bonne option et le bon nombre d'article dans le panier
 // une fonction pour gerer addToCart en envoyant dans le LS les données utilisateurs
-// const addToCart = () => {
-//   let kanapsArray = []
-//   // recuperation de la couleur selectionné
-
-//   /*
-//   const colorChoice = document.querySelector('option')
-//   document.addEventListener('DOMContentLoaded', function () {
-//     document.querySelector('select[name="color-select"]').onchange = changeEventHandler
-//   }, false)
-//   function changeEventHandler (event) {
-//     if (!event.target.value) alert('Veuillez choisir une couleur SVP')
-//     else colorChoice = event.target.value
-//   }
-//   */
-
-//   // au click sur le bouton récuperer les données utilisateurs
-//   const addToCartButton = document.querySelector('button')
-//   addToCartButton.addEventListener('click', () => {
-//     const colorSelected = document.getElementById('colors').value
-//     if (
-//       inputQuantity.value > 0 &&
-//       inputQuantity.value < 100 &&
-//       colorSelected !== null
-//     ) {
-//       const product = {
-//         dataId: id,
-//         color: colorSelected,
-//         quantity: parseFloat(inputQuantity.value)
-//       }
-//       if (localStorage.getItem(product) !== null) {
-//         kanapsArray = JSON.parse(localStorage.getItem('product'))
-//       } else {
-//         kanapsArray.push(product)
-//         localStorage.setItem('product', JSON.stringify(kanapsArray))
-//       }
-//     } else {
-//       alert("ceci n'est pas un nombre compris entre 1 et 100")
-//     }
-//   })
-// }
+getUserData()
+const addToCart = () => {
+  let kanapsArray = []
+  const addToCartButton = document.querySelector('button')
+  addToCartButton.addEventListener('click', () => {
+    if (
+      userData.quantity > 0 &&
+      userData.quantity < 100 &&
+      userData.color !== null
+    ) {
+      if (localStorage.getItem(userData) !== null) {
+        kanapsArray = JSON.parse(localStorage.getItem('userData'))
+      } else {
+        kanapsArray.push(userData)
+        localStorage.setItem('userData', JSON.stringify(kanapsArray))
+      }
+    } else {
+      alert("Veuillez renseigner une couleur ou un nombre d'article compris entre un et 100")
+    }
+  })
+}
 
 const main = () => {
   displayKanap()
-  dataInput()
-  // addToCart()
+  addToCart()
 }
 
 main()
